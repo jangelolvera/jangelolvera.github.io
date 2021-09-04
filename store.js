fbddcfe8d9c738f0ab6efb76eeb001d2c9122e18
jangelolvera <70440610+jangelolvera@users.noreply.github.com>
2021-09-03 23:17:47 -0500

    Add files via upload

---
+++ store.js
@@ -0,0 +1,35 @@
+let totalItems=0;
+let boxvalue;
+const countDisplay= document.querySelector('#total');
+
+class Store {
+    /**
+     * @param {string[]} items
+     */
+    constructor() {
+        this.items = [];
+    }
+
+    addItem(item) {
+        this.items.push(item);
+        console.log(items);
+    
+    }
+
+    getItems() {
+        for(let i=0; i<this.items.length(); i++){
+            this.items[i];
+        }
+    }
+
+    getCount() {
+        countDisplay.textContent=this.items.length();
+    }
+
+    exists(item) {
+        this.items.includes(item);
+    }
+}
+
+// Do not modify this
+export default Store;
